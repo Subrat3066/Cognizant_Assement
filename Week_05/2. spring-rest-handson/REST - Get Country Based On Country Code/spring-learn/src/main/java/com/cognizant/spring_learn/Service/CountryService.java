0bf95caedd54259d39e5b8a891bf02f44bf1f374
Subrat3066 <subratkumar3066@gmail.com>
2026-07-23 00:52:23 +0530

    adding week_05 handson

---
+++ Week_05/2. spring-rest-handson/REST - Get Country Based On Country Code/spring-learn/src/main/java/com/cognizant/spring_learn/Service/CountryService.java
@@ -0,0 +1,28 @@
+package com.cognizant.spring_learn.Service;
+
+import com.cognizant.spring_learn.Model.Country;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CountryService {
+
+    public Country getCountry(String code) {
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+
+        List<Country> countries = (List<Country>) context.getBean("countryList");
+
+        for (Country country : countries) {
+            if (country.getCode().equalsIgnoreCase(code)) {
+                context.close();
+                return country;
+            }
+        }
+
+        context.close();
+        return null;
+    }
+}
